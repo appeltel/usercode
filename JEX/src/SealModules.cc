675e0a5f6cf2e743114edbf657ba88528e42d1bb
appeltel <>
2010-07-21 20:50:21 +0000

    Initial Version

---
+++ JEX/src/SealModules.cc
@@ -0,0 +1,12 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "Appeltel/JEX/interface/HiTrackListMerger.h"
+
+
+using cms::HiTrackListMerger;
+
+
+DEFINE_FWK_MODULE(HiTrackListMerger);
+
