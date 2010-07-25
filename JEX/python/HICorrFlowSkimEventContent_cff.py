c0d84eaae4de4d78b0f0bb7f5852b19b6b8754fa
appeltel <>
2010-07-25 14:31:01 +0000

    initial revision

---
+++ JEX/python/HICorrFlowSkimEventContent_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+
+HICorrFlowEventContent = cms.PSet(
+    outputCommands = cms.untracked.vstring( 'drop *',
+        'keep recoTracks_hiSelectedTracks_*_*', 
+        'keep recoTracks_hiLowPtPixelTracks_*_*',
+        'keep *_hiSelectedVertex_*_*',
+        'keep *_hiCentrality_*_*',
+        'keep *_hiEvtPlane_*_*',
+        'keep recoCaloJets_*_*_*', 
+        'keep recoRecoChargedCandidates_*_*_*',
+        'keep CaloTowersSorted_towerMaker_*_*'
+    )
+)
+ 
