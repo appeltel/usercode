0829299ede5db0549ce81ca7e7c381108afe81ad
appeltel <>
2010-07-29 13:46:40 +0000

    fixed directory names

--- hackedSiStripDigitizer/hackTheDigitizer.sh
+++ hackedSiStripDigitizer/hackTheDigitizer.sh
@@ -5,10 +5,10 @@ cd $CMSSW_BASE/src
 cvs co UserCode/Appeltel/hackedSiStripDigitizer
 cvs co SimTracker/SiStripDigitizer
 
-mv UserCode/Appeltel/hackedSiStripDigitizer/interface/SiStripDigitizer.h SimTracker/SiStripDigitizer/interface/SiStripDigitizer.h
-mv UserCode/Appeltel/hackedSiStripDigitizer/plugins/SiStripDigitizer.cc SimTracker/SiStripDigitizer/plugins/SiStripDigitizer.cc
-mv UserCode/Appeltel/hackedSiStripDigitizer/interface/SiStripDigitizerAlgorithm.h SimTracker/SiStripDigitizer/interface/SiStripDigitizerAlgorithm.h
-mv UserCode/Appeltel/hackedSiStripDigitizer/src/SiStripDigitizerAlgorithm.h SimTracker/SiStripDigitizer/src/SiStripDigitizerAlgorithm.h
+mv UserCode/Appeltel/hackedSiStripDigitizer/SiStripDigitizer.h SimTracker/SiStripDigitizer/interface/SiStripDigitizer.h
+mv UserCode/Appeltel/hackedSiStripDigitizer/SiStripDigitizer.cc SimTracker/SiStripDigitizer/plugins/SiStripDigitizer.cc
+mv UserCode/Appeltel/hackedSiStripDigitizer/SiStripDigitizerAlgorithm.h SimTracker/SiStripDigitizer/interface/SiStripDigitizerAlgorithm.h
+mv UserCode/Appeltel/hackedSiStripDigitizer/SiStripDigitizerAlgorithm.h SimTracker/SiStripDigitizer/src/SiStripDigitizerAlgorithm.h
 
 scram b
 
