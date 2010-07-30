8686deb202e98610fe235252853d767cbae46cde
appeltel <>
2010-07-30 08:33:53 +0000

    Added integrated ADC histograms

--- SiStripCMNAnalyzer/src/SiStripCMNAnalyzer.cc
+++ SiStripCMNAnalyzer/src/SiStripCMNAnalyzer.cc
@@ -83,6 +83,7 @@ class SiStripCMNAnalyzer : public edm::EDAnalyzer {
 
       inline float median( std::vector<int16_t>& sample);
       inline float percentile ( std::vector<int16_t>& sample, double pct);
+      inline float integrateADC ( std::vector<int16_t>& sample, double offset);
 
       void processRaw(const edm::InputTag&, const edm::DetSetVector<SiStripRawDigi>&, std::vector<edm::DetSet<SiStripDigi> >& );
       std::auto_ptr<SiStripRawProcessingAlgorithms> algorithms;
@@ -124,6 +125,18 @@ class SiStripCMNAnalyzer : public edm::EDAnalyzer {
 
       TGraph* stripCount_[10];
       TGraph* stripSignalCount_[10];
+
+      // Histograms of integrated ADC  on each APV
+
+      TH1F* totalADCMedian_;
+      TH1F* totalADCPer25_;
+
+      // for comparison to actual integrated ADC from signal only with perfect baseline
+
+      TH1F* totalADCSignal_;
+      TH1F* totalADCMedianDiff_;
+      TH1F* totalADCPer25Diff_;
+
 
 
       bool firstEvent;
@@ -171,6 +184,18 @@ SiStripCMNAnalyzer::SiStripCMNAnalyzer(const edm::ParameterSet& iConfig)
   TECMedians_=fs->make<TH1F>("TECMedians","Median ADC count of APV25 for the TEC", 500, 0., 200.);
   TEC25ths_=fs->make<TH1F>("TEC25ths","25th percentile ADC count of APV25 for the TEC", 500, 0., 200.);
   TECGens_=fs->make<TH1F>("TECGens","Expected CMN Distribution for the TEC", 500, 0., 200.);
+
+  totalADCMedian_=fs->make<TH1F>("totalADCMedian","Integrated ADC count for each APV25 after Median CMN subtraction",500,0.,10000.);
+  totalADCPer25_=fs->make<TH1F>("totalADCPer25","Integrated ADC count for each APV25 after 25th Percentile CMN subtraction",500,0.,10000.);
+  if(doNoiseAndSignal)
+  {
+    totalADCSignal_=fs->make<TH1F>("totalADCSignal","Integrated ADC count for each APV25 from true SimHits only",500,0.,10000.);
+    totalADCMedianDiff_=fs->make<TH1F>("totalADCMedianDiff",
+      "Difference between median subtracted integrated ADC count and ADC count from signal",500,-3000.,2000.);
+    totalADCPer25Diff_=fs->make<TH1F>("totalADCPer25Diff",
+      "Difference between 25th percentile subtracted integrated ADC count and ADC count from signal",500,-3000.,2000.);
+  }    
+  
 
   for( int i = 0; i<10; i++)
   {
@@ -287,17 +312,24 @@ SiStripCMNAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
     // MEDIAN ALGORITHM
 
     std::vector<int16_t> tmp;  tmp.reserve(128);  
+    std::vector<int16_t> tmpSignal;  tmpSignal.reserve(128);  
     std::vector<int16_t>::iterator  
         strip( processedRawDigis.begin() ), 
         signal(signalDigis.begin() ),
         end(   processedRawDigis.end()   ),
-        endAPV;
+        endAPV, 
+        endAPVSignal;
   
     while( strip < end ) {
-      endAPV = strip+128; tmp.clear();
+      endAPV = strip+128;
+      endAPVSignal = signal+128; 
+      tmp.clear(); tmpSignal.clear();
       tmp.insert(tmp.end(),strip,endAPV);
+      tmpSignal.insert(tmpSignal.end(),signal,endAPVSignal);
       const float offset = median(tmp);
       const float per25 = percentile(tmp,25.);
+
+      // Fill offset plots for all algorthims for all detectors and for TIB, TOB, TID, TEC
 
       allMedians_->Fill( offset );
       all25ths_->Fill( per25 );
@@ -326,6 +358,20 @@ SiStripCMNAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
           break;
       }      
       medvs25_->Fill( offset, per25);
+
+      // Integrated ADC after baseline subtraction plots
+      float intMed = integrateADC(tmp, offset );
+      float intPer25 = integrateADC(tmp, per25 );
+
+      totalADCMedian_->Fill( intMed );
+      totalADCPer25_->Fill( intPer25 );
+      if ( doNoiseAndSignal )
+      {
+         float intSignal = integrateADC(tmpSignal, 0. );
+         totalADCSignal_->Fill( intSignal );
+         totalADCMedianDiff_->Fill( intMed - intSignal );
+         totalADCPer25Diff_->Fill( intPer25 - intSignal );
+      }
 
       // make plots of the 10 wonkiest strips in the first event
 
@@ -382,6 +428,16 @@ inline float SiStripCMNAnalyzer::percentile( std::vector<int16_t>& sample, doubl
   std::nth_element(sample.begin(), mid, sample.end());
   return *mid;
 } 
+
+inline float SiStripCMNAnalyzer::integrateADC ( std::vector<int16_t>& sample, double offset ) {
+  float sum = 0.;
+  for( std::vector<int16_t>::iterator it = sample.begin(); it != sample.end(); ++it)
+  { 
+    sum += *it-offset > 0. ? *it-offset : 0. ;
+  }
+  return sum;
+}
+
 
 
 //define this as a plug-in
