38f1e2f5c4c4fc109472941007b3ff8d87c7b5e2
appeltel <>
2010-10-22 20:30:02 +0000

    added wide tracks and nosplit merged tracks

--- JEX/python/HICorrFlowSkimEventContent_cff.py
+++ JEX/python/HICorrFlowSkimEventContent_cff.py
@@ -5,6 +5,8 @@ HICorrFlowEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring( 'drop *',
         'keep recoTracks_hiSelectedTracks_*_*', 
         'keep recoTracks_hiLowPtPixelTracks_*_*',
+        'keep recoTracks_hiLowPtWideCutPixelTracks_*_*',
+        'keep recoTracks_hiMergedNoPtSplitTracks_*_*',
         'keep *_hiSelectedVertex_*_*',
         'keep *_hiCentrality_*_*',
         'keep *_hiEvtPlane_*_*',
