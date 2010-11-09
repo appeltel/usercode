5f2049a2bf0c2cdab3a03cc4cbb2d086ca3d7851
appeltel <>
2010-11-09 13:07:14 +0000

    Simplify to use new cffs

--- PixelTracksRun2010/test/corrFlowSkim_data.py
+++ PixelTracksRun2010/test/corrFlowSkim_data.py
@@ -14,9 +14,9 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.EventContent.EventContentHeavyIons_cff')
 
-process.load('Appeltel.PixelTracksRun2010.HILowPtPixelTracks_cfi')
-process.load('Appeltel.PixelTracksRun2010.HILowPtWideCutPixelTracks_cfi')
-process.load('Appeltel.PixelTracksRun2010.HIInvertedLowPtPixelTracks_cfi')
+process.load('Appeltel.PixelTracksRun2010.HiLowPtPixelTracksFromReco_cff')
+process.load('Appeltel.PixelTracksRun2010.HiMultipleMergedTracks_cff')
+process.load('Appeltel.PixelTracksRun2010.HiTrackCandidates_cff')
 process.load('Appeltel.PixelTracksRun2010.HICorrFlowSkimEventContent_cff')
 
 
@@ -44,120 +44,22 @@ process.output = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string( 'file:/scratch/appelte1/skim_data_test.root')
 )
 
-
 #Trigger Selection
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 process.hltMinBiasHF = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
 process.hltMinBiasHF.HLTPaths = ["HLT_HIMinBiasHF"]
 
-
-# Charged Candidates
-
-process.allPixelTracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiLowPtPixelTracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allInvertedLowPtPixelTracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiInvertedLowPtPixelTracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allSelectedTracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiSelectedTracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allMergedNoPtSplitTracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiMergedNoPtSplitTracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allMergedPtSplit09Tracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiMergedPtSplit09Tracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allMergedPtSplit12Tracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiMergedPtSplit12Tracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allMergedPtSplit15Tracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiMergedPtSplit15Tracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-process.allMergedPtSplit18Tracks = cms.EDProducer("ConcreteChargedCandidateProducer",
-    src = cms.InputTag("hiMergedPtSplit18Tracks"), # or whatever you call the more tightly selected collection
-    particleType = cms.string('pi+')
-)
-
-
-import Appeltel.PixelTracksRun2010.hiTrackListMerger_cfi
-
-process.hiMergedNoPtSplitTracks = Appeltel.PixelTracksRun2010.hiTrackListMerger_cfi.hiTrackListMerger.clone(
-    TrackProducer1 = 'hiSelectedTracks',
-    TrackProducer2 = 'hiLowPtPixelTracks',
-    promoteTrackQuality = False,  
-    allowFirstHitShare = False   
-)
-
-process.hiMergedPtSplit09Tracks = process.hiMergedNoPtSplitTracks.clone(
-    trackCollection2MaxPt = 0.9,
-    trackCollection1MinPt = 0.9
-)
-
-process.hiMergedPtSplit12Tracks = process.hiMergedNoPtSplitTracks.clone(
-    trackCollection2MaxPt = 1.2,
-    trackCollection1MinPt = 1.2
-)
-
-process.hiMergedPtSplit15Tracks = process.hiMergedNoPtSplitTracks.clone(
-    trackCollection2MaxPt = 1.5,
-    trackCollection1MinPt = 1.5
-)
-
-process.hiMergedPtSplit18Tracks = process.hiMergedNoPtSplitTracks.clone(
-    trackCollection2MaxPt = 1.8,
-    trackCollection1MinPt = 1.8
-)
-
-
 # Other statements
 process.GlobalTag.globaltag = 'GR10_P_V12::All'
-
-
 
 # Path and EndPath definitions
 
 process.filter_step = cms.Path( process.hltMinBiasHF )
 
-process.lowptpixel_step = cms.Path(
-    process.siPixelRecHits * 
-    process.hiLowPtPixelTracks *
-    process.hiLowPtWideCutPixelTracks *
-    process.hiInvertedLowPtPixelTracks 
-)
-process.merge_step = cms.Path(
-    process.hiMergedNoPtSplitTracks *
-    process.hiMergedPtSplit09Tracks *
-    process.hiMergedPtSplit12Tracks *
-    process.hiMergedPtSplit15Tracks *
-    process.hiMergedPtSplit18Tracks
-)
-process.candidates_step = cms.Path(
-    process.allPixelTracks *
-    process.allInvertedLowPtPixelTracks *
-    process.allSelectedTracks *
-    process.allMergedNoPtSplitTracks *
-    process.allMergedPtSplit09Tracks *
-    process.allMergedPtSplit12Tracks *
-    process.allMergedPtSplit15Tracks *
-    process.allMergedPtSplit18Tracks 
-)
-
+process.lowptpixel_step = cms.Path(process.lowPtPixelTrackReco)
+process.merge_step = cms.Path(process.multipleMergedTracks)
+process.candidates_step = cms.Path( process.hiAllTrackCandidates)
 process.endjob_step = cms.Path(process.endOfProcess)
 process.out_step = cms.EndPath(process.output)
 
