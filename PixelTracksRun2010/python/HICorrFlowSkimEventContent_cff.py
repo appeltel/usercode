274c6c0a927a5b00f28418456dd46a592824f217
appeltel <>
2010-11-17 22:44:24 +0000

    add genparticles, better selections

--- PixelTracksRun2010/python/HICorrFlowSkimEventContent_cff.py
+++ PixelTracksRun2010/python/HICorrFlowSkimEventContent_cff.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 
 HICorrFlowEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring( 'drop *',
+        'keep recoTracks_hiGoodTracks_*_*',
+        'keep recoTracks_hiQualityMergedTracks_*_*',
         'keep recoTracks_hiSelectedTracks_*_*', 
         'keep recoTracks_hiLowPtPixelTracks_*_*',
         'keep recoTracks_hiLowPtWideCutPixelTracks_*_*',
@@ -13,7 +15,10 @@ HICorrFlowEventContent = cms.PSet(
         'keep recoCaloJets_*_*_*', 
         'keep recoRecoChargedCandidates_*_*_*',
         'keep CaloTowersSorted_towerMaker_*_*',
-        'keep recoCaloClusters_*_*_*'
+        'keep recoCaloClusters_*_*_*',
+        'keep *_hiGenParticles_*_*',
+        'keep *_generator_*_*',
+        'keep recoGenJets_*_*'
     )
 )
  
