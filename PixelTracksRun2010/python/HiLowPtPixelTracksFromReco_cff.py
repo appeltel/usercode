4fa9fddda8f4314664ed03854b180f8057db240e
appeltel <>
2010-11-29 13:35:50 +0000

    comment out cluster removal

--- PixelTracksRun2010/python/HiLowPtPixelTracksFromReco_cff.py
+++ PixelTracksRun2010/python/HiLowPtPixelTracksFromReco_cff.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
-from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi import *
-from RecoTracker.TkSeedingLayers.PixelLayerTriplets_cfi import *
 from Appeltel.PixelTracksRun2010.HILowPtPixelTracks_cfi import *
 from Appeltel.PixelTracksRun2010.HIGoodPixelTracks_cfi import *
 from Appeltel.PixelTracksRun2010.HILowPtWideCutPixelTracks_cfi import *
@@ -22,6 +20,15 @@ goodPixelTrackReco = cms.Sequence(
     hiGoodPixelTracks
 )
 
+conformalPixelTrackReco = cms.Sequence(
+    siPixelRecHits *
+    hiConformalPixelTracks 
+)
+
+#
+# This code is for secondary pixel tracking by 
+# cluster removal. Please leave in the file as
+# comments as we may choose to use it later.
 #
 # If making conformal pixel tracks, make sure that
 # you set the quality bit for hiGoodTracks!
@@ -29,42 +36,43 @@ goodPixelTrackReco = cms.Sequence(
 # You will also need to rerun heavy ion tracking
 # reconstruction in order to propagate the trajectories
 #
-
-firstStepFilter = cms.EDProducer("QualityFilter",
-    TrackQuality = cms.string('tight'),
-    recTracks = cms.InputTag("hiGoodTracks")
-)
-
-secClusters = cms.EDProducer("TrackClusterRemover",
-    trajectories = cms.InputTag("firstStepFilter"),
-    pixelClusters = cms.InputTag("siPixelClusters"),
-    stripClusters = cms.InputTag("siStripClusters"),
-    Common = cms.PSet(
-        maxChi2 = cms.double(1000.0)
-    )
-)
-
-
-secPixelRecHits = siPixelRecHits.clone(
-    src = cms.InputTag("secClusters")
-    )
-
-secStripRecHits = siStripMatchedRecHits.clone(
-    ClusterProducer = 'secClusters'
-    )
-
-secpixellayertriplets = pixellayertriplets.clone()
-secpixellayertriplets.ComponentName = cms.string('SecPixelLayerTriplets')
-secpixellayertriplets.BPix.HitProducer = cms.string('secPixelRecHits')
-secpixellayertriplets.FPix.HitProducer = cms.string('secPixelRecHits')
-
-hiConformalPixelTracks.OrderedHitsFactoryPSet.SeedingLayers = cms.string("SecPixelLayerTriplets")
-
-conformalPixelTrackReco = cms.Sequence(
-    siPixelRecHits *
-    siStripMatchedRecHits *
-    firstStepFilter *
-    secClusters *
-    secPixelRecHits *
-    hiConformalPixelTracks 
-)
+#from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi import *
+#from RecoTracker.TkSeedingLayers.PixelLayerTriplets_cfi import *
+#
+#firstStepFilter = cms.EDProducer("QualityFilter",
+#    TrackQuality = cms.string('tight'),
+#    recTracks = cms.InputTag("hiGoodTracks")
+#)
+#
+#secClusters = cms.EDProducer("TrackClusterRemover",
+#    trajectories = cms.InputTag("firstStepFilter"),
+#    pixelClusters = cms.InputTag("siPixelClusters"),
+#    stripClusters = cms.InputTag("siStripClusters"),
+#    Common = cms.PSet(
+#        maxChi2 = cms.double(1000.0)
+#    )
+#)
+#
+#secPixelRecHits = siPixelRecHits.clone(
+#    src = cms.InputTag("secClusters")
+#    )
+#
+#secStripRecHits = siStripMatchedRecHits.clone(
+#    ClusterProducer = 'secClusters'
+#    )
+#
+#secpixellayertriplets = pixellayertriplets.clone()
+#secpixellayertriplets.ComponentName = cms.string('SecPixelLayerTriplets')
+#secpixellayertriplets.BPix.HitProducer = cms.string('secPixelRecHits')
+#secpixellayertriplets.FPix.HitProducer = cms.string('secPixelRecHits')
+#
+#hiConformalPixelTracks.OrderedHitsFactoryPSet.SeedingLayers = cms.string("SecPixelLayerTriplets")
+#
+#conformalPixelTrackReco = cms.Sequence(
+#    siPixelRecHits *
+#    siStripMatchedRecHits *
+#    firstStepFilter *
+#    secClusters *
+#    secPixelRecHits *
+#    hiConformalPixelTracks 
+#)
