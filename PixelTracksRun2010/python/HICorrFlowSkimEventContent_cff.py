36536899deb9757050e380efb4323c18c9192857
appeltel <>
2010-11-29 13:38:53 +0000

    add conformal and good merged tracks

--- PixelTracksRun2010/python/HICorrFlowSkimEventContent_cff.py
+++ PixelTracksRun2010/python/HICorrFlowSkimEventContent_cff.py
@@ -7,6 +7,8 @@ HICorrFlowEventContent = cms.PSet(
         'keep recoTracks_hiQualityMergedTracks_*_*',
         'keep recoTracks_hiSelectedTracks_*_*', 
         'keep recoTracks_hiGoodPixelTracks_*_*',
+        'keep recoTracks_hiConformalPixelTracks_*_*',
+        'keep recoTracks_hiGoodMergedTracks_*_*',
         'keep recoTracks_hiLowPtPixelTracks_*_*',
         'keep recoTracks_hiLowPtWideCutPixelTracks_*_*',
         'keep recoTracks_hiMergedNoPtSplitTracks_*_*',
