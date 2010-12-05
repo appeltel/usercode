dc5c1f9238cae0008197cf38b60b01b2213bdafe
appeltel <>
2010-12-05 15:36:15 +0000

    changed parameter and fit scheme

--- MergedTrackCorrections/src/MergedTrackCorrector.cc
+++ MergedTrackCorrections/src/MergedTrackCorrector.cc
@@ -11,6 +11,39 @@
 #include <fstream>
 using namespace std;
 
+/********************************************************
+ 
+Fit functions used are
+
+- Efficiency
+
+- [0]/(1+exp([1]*x+[2])) + [3]*x^2
+- pol2
+- pol2
+
+- Fake rate
+
+- [0]/(1+exp([1]*x+[2])) + [3]*x^2
+- pol2
+- pol3
+
+
+Eta and Centrality binning are done in the following way,
+
+- 2.4~-1.0 : bin width of 0.2
+- 1.0~1.0: bin width of 0.4
+- 1.0~2.4: bin width of 0.2
+
+==> 19 bins (0~18)
+
+- 0~10%
+- 10~30%
+- 30~100%
+
+==> 3 bins (0~3)
+
+
+********************************************************/
 
 class MergedTrackCorrector
 {
@@ -44,7 +77,7 @@ MergedTrackCorrector::~MergedTrackCorrector(){}
 
 void MergedTrackCorrector::loadEffic()
 {
-  loadEffic("Appeltel/MergedTrackCorrections/data/CorrTable_Bass_100k_effic.par");
+  loadEffic("Appeltel/MergedTrackCorrections/data/EFF_validation3D_bass_hiBadgerTracks_v2.par");
 }
 
 void MergedTrackCorrector::loadEffic( const char * filename )
@@ -62,7 +95,8 @@ void MergedTrackCorrector::loadEffic( const char * filename )
     inFile >> eta;
     inFile >> cent;
 
-    for( int par = 0; par<4; par++ )
+    int parmax = (pt == 0) ? 3 : 4;
+    for( int par = 0; par<parmax; par++ )
       inFile >> effic_[pt][eta][cent][par];
   }
 
@@ -71,7 +105,7 @@ void MergedTrackCorrector::loadEffic( const char * filename )
 
 void MergedTrackCorrector::loadFakes()
 {
-  loadFakes("Appeltel/MergedTrackCorrections/data/CorrTable_Bass_100k_fakes.par");
+  loadFakes("Appeltel/MergedTrackCorrections/data/FAK_validation3D_bass_hiBadgerTracks_v2.par");
 }
 
 void MergedTrackCorrector::loadFakes( const char * filename )
@@ -89,7 +123,8 @@ void MergedTrackCorrector::loadFakes( const char * filename )
     inFile >> eta;
     inFile >> cent;
 
-    for( int par = 0; par<4; par++ )
+    int parmax = (pt==1) ? 3 : 4;
+    for( int par = 0; par<parmax; par++ )
       inFile >> fake_[pt][eta][cent][par];
   }
 
@@ -103,11 +138,26 @@ float MergedTrackCorrector::weight( float pt, float eta, int cbin )
    return  ( 1. - fakeRate(pt, eta, cbin) )/ effic( pt, eta, cbin );
 }
 
-float MergedTrackCorrector::effic( float pt, float eta, int cbin )
+float MergedTrackCorrector::fakeRate( float pt, float eta, int centbin )
 {
-   int etabin = (int) ( (eta+3.)*2. );
-   int ptbin;
-   cbin = cbin / 4;
+
+   int etabin = 0;
+   if ( eta < -1.0 ) 
+     etabin = (int) (eta+2.4)*5;
+   if ( eta >= -1.0 && eta < 1.0 ) 
+     etabin = (int) ( (eta+1.0)*2.5 + 7 );
+   if ( eta >= 1.0 )
+     etabin = (int) ( (eta-1.0)*5 + 12 );
+
+   int cbin = 0;
+   if ( centbin < 4)
+     cbin = 0;
+   if ( centbin >= 4 && centbin < 12 )
+     cbin = 1;
+   if ( centbin >= 12)
+     cbin = 2;
+
+   int ptbin = 0;
    if( pt < 1.5 )   
      ptbin = 0;
    if( pt >= 1.5 && pt < 1.8 )
@@ -115,19 +165,51 @@ float MergedTrackCorrector::effic( float pt, float eta, int cbin )
    if( pt >= 1.8)
      ptbin = 2;
 
-   return effic_[ptbin][etabin][cbin][0]*pt*pt*pt + 
-          effic_[ptbin][etabin][cbin][1]*pt*pt + 
-          effic_[ptbin][etabin][cbin][2]*pt + 
-          effic_[ptbin][etabin][cbin][3]; 
+   if ( ptbin == 2)
+     return fake_[ptbin][etabin][cbin][0]*pt*pt*pt + 
+            fake_[ptbin][etabin][cbin][1]*pt*pt + 
+            fake_[ptbin][etabin][cbin][2]*pt + 
+            fake_[ptbin][etabin][cbin][3]; 
+
+   if ( ptbin == 1)
+     return fake_[ptbin][etabin][cbin][0]*pt*pt + 
+            fake_[ptbin][etabin][cbin][1]*pt + 
+            fake_[ptbin][etabin][cbin][2];
+
+   if ( ptbin == 0)
+   {
+      float par0 = fake_[ptbin][etabin][cbin][0]; 
+      float par1 = fake_[ptbin][etabin][cbin][1]; 
+      float par2 = fake_[ptbin][etabin][cbin][2]; 
+      float par3 = fake_[ptbin][etabin][cbin][3]; 
+      
+      return par0 / ( 1. + exp(par1*pt+par2)) + par3*pt*pt;
+   }
+
+   return 0.0;
 }    
   
 
-
-float MergedTrackCorrector::fakeRate( float pt, float eta, int cbin )
+float MergedTrackCorrector::effic( float pt, float eta, int centbin )
 {
-   int etabin = (int) ( (eta+3.)*2. );
-   int ptbin;
-   cbin = cbin / 4;
+
+   int etabin = 0;
+   if ( eta < -1.0 ) 
+     etabin = (int) (eta+2.4)*5;
+   if ( eta >= -1.0 && eta < 1.0 ) 
+     etabin = (int) ( (eta+1.0)*2.5 + 7 );
+   if ( eta >= 1.0 )
+     etabin = (int) ( (eta-1.0)*5 + 12 );
+
+   int cbin = 0;
+   if ( centbin < 4)
+     cbin = 0;
+   if ( centbin >= 4 && centbin < 12 )
+     cbin = 1;
+   if ( centbin >= 12)
+     cbin = 2;
+
+   int ptbin = 0;
    if( pt < 1.5 )   
      ptbin = 0;
    if( pt >= 1.5 && pt < 1.8 )
@@ -135,11 +217,25 @@ float MergedTrackCorrector::fakeRate( float pt, float eta, int cbin )
    if( pt >= 1.8)
      ptbin = 2;
 
-   return fake_[ptbin][etabin][cbin][0]*pt*pt*pt + 
-          fake_[ptbin][etabin][cbin][1]*pt*pt + 
-          fake_[ptbin][etabin][cbin][2]*pt + 
-          fake_[ptbin][etabin][cbin][3]; 
+
+   if ( ptbin >0)
+     return effic_[ptbin][etabin][cbin][0]*pt*pt + 
+            effic_[ptbin][etabin][cbin][1]*pt + 
+            effic_[ptbin][etabin][cbin][2];
+
+   if ( ptbin == 0)
+   {
+      float par0 = effic_[ptbin][etabin][cbin][0]; 
+      float par1 = effic_[ptbin][etabin][cbin][1]; 
+      float par2 = effic_[ptbin][etabin][cbin][2]; 
+      float par3 = effic_[ptbin][etabin][cbin][3]; 
+      
+      return par0 / ( 1. + exp(par1*pt+par2)) + par3*pt*pt;
+   }
+
+   return 1.0;
 }    
+
    
   
 
