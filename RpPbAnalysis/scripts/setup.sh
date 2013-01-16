c168ed6c24a2ac8e8e0391a4216b835d9d7a7de2
appeltel <>
2013-01-16 21:20:26 +0000

    add forest

--- RpPbAnalysis/scripts/setup.sh
+++ RpPbAnalysis/scripts/setup.sh
@@ -7,4 +7,7 @@ cvs co -r pPbProd_v1 DataFormats/HeavyIonEvent
 cvs co -r pPbProd_v1 RecoHI/HiCentralityAlgos
 cvs co -r pPbProd_v1 HeavyIonsAnalysis/Configuration
 
+# forest
+cvs co -d HiForest -r HiForest_V2_03_01 UserCode/CmsHi/HiForest/V2
+
 scram b
