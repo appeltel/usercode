ea65b88ca7f87ad6f3c06b779100f13a8cb86d91
appeltel <>
2013-01-19 19:28:01 +0000

    new tags

--- RpPbAnalysis/scripts/setup.sh
+++ RpPbAnalysis/scripts/setup.sh
@@ -3,9 +3,9 @@
 cd $CMSSW_BASE/src
 
 # event selection and centrality 
-cvs co -r pPbProd_v1 DataFormats/HeavyIonEvent
-cvs co -r pPbProd_v1 RecoHI/HiCentralityAlgos
-cvs co -r pPbProd_v1 HeavyIonsAnalysis/Configuration
+cvs co -r pPbProd_v04 DataFormats/HeavyIonEvent
+cvs co -r pPbProd_v04 RecoHI/HiCentralityAlgos
+cvs co -r pPbProd_v03 HeavyIonsAnalysis/Configuration
 
 # forest
 cvs co -d HiForest -r HiForest_V2_03_01 UserCode/CmsHi/HiForest/V2
