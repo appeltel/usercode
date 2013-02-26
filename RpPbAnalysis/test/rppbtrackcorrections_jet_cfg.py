25f68cb84e411786bd5f74aa6873b5b8ccc36bf6
appeltel <>
2013-02-26 18:08:54 +0000

    add calo jets

--- RpPbAnalysis/test/rppbtrackcorrections_jet_cfg.py
+++ RpPbAnalysis/test/rppbtrackcorrections_jet_cfg.py
@@ -62,22 +62,90 @@ process.PFTowers.src = cms.InputTag("particleFlow")
 
 process.GlobalTag.globaltag = 'STARTHI53_V25::All'
 
-process.pPbTrkCorr.occByCentrality = cms.bool(False)
-process.pPbTrkCorr.occByJetEt = cms.bool(True)
-process.pPbTrkCorr.occBins = cms.vdouble( 0., 40.,80.,120.,160.,200.,300.,500.)
+#process.pPbTrkCorr.occByCentrality = cms.bool(False)
+#process.pPbTrkCorr.occByJetEt = cms.bool(True)
+#process.pPbTrkCorr.occBins = cms.vdouble( 0., 40.,80.,120.,160.,200.,300.,500.)
+
+process.trkCorr_akPu3PF_Et80dR03 = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3PFpatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.3),
+    invertJetCuts = cms.bool(False)
+)
+
+process.trkCorr_akPu3PF_Et80dR08 = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3PFpatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.8),
+    invertJetCuts = cms.bool(False)
+)
+
+process.trkCorr_akPu3PF_Et80dR03inv = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3PFpatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.3),
+    invertJetCuts = cms.bool(True)
+)
+
+process.trkCorr_akPu3PF_Et80dR08inv = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3PFpatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.8),
+    invertJetCuts = cms.bool(True)
+)
+
+process.trkCorr_akPu3Calo_Et80dR03 = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3CalopatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.3),
+    invertJetCuts = cms.bool(False)
+)
+
+process.trkCorr_akPu3Calo_Et80dR08 = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3CalopatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.8),
+    invertJetCuts = cms.bool(False)
+)
+
+process.trkCorr_akPu3Calo_Et80dR03inv = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3CalopatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.3),
+    invertJetCuts = cms.bool(True)
+)
+
+process.trkCorr_akPu3Calo_Et80dR08inv = process.pPbTrkCorr.clone(
+    jetSrc = cms.InputTag('akPu3CalopatJets'),
+    applyJetCuts = cms.bool(True),
+    jetRadius = cms.double(0.8),
+    invertJetCuts = cms.bool(True)
+)
+
+
 
 process.jetReco= cms.Sequence(
     process.PFTowers *
     process.akPu3PFJets*
     process.akPu3PFcorr*
-    process.akPu3PFpatJets
+    process.akPu3PFpatJets*
+    process.akPu3CaloJets*
+    process.akPu3Calocorr*
+    process.akPu3CalopatJets
 )
 
 process.p = cms.Path( 
-                      process.siPixelRecHits *
-                      process.pACentrality *
-                      process.jetReco *
-                      process.tpRecoAssocGeneralTracks *
-                      process.pPbTrkCorr 
+    process.siPixelRecHits *
+    process.pACentrality *
+    process.jetReco *
+    process.tpRecoAssocGeneralTracks *
+    process.trkCorr_akPu3PF_Et80dR03 * 
+    process.trkCorr_akPu3PF_Et80dR08 * 
+    process.trkCorr_akPu3PF_Et80dR03inv * 
+    process.trkCorr_akPu3PF_Et80dR08inv * 
+    process.trkCorr_akPu3Calo_Et80dR03 * 
+    process.trkCorr_akPu3Calo_Et80dR08 * 
+    process.trkCorr_akPu3Calo_Et80dR03inv * 
+    process.trkCorr_akPu3Calo_Et80dR08inv  
 )
 
